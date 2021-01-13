3c64c18e7402706ea3cb2db103998720b80a11b0
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-13 17:32:12 -0500

    modified function for passing tests

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (letters.length <= 0) {
+    return undefined;
+  }
 
-module.exports = wordSearch
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  let newArr = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    let result = [];
+    for (let j = 0; j < letters.length; j++) {
+      result.push(letters[j][i]);
+    }
+    newArr.push(result);
+  }
+
+  const verticalJoin = newArr.map((letter) => letter.join(""));
+
+  for (const item of verticalJoin) {
+    if (item.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+
+
+module.exports = wordSearch;
